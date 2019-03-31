f1f3e45f2fa051ed1c18733d8ce1428b17b6da3d
gustavolbs <gustavo.santos@ccc.ufcg.edu.br>
2019-03-30 23:43:26 -0300

    Commit by gustavolbs at sábado 03/30/19 2019 23:43:26

---
+++ README.md
@@ -0,0 +1,22 @@
+# Python-Telegram-Bot
+> First of all, verify if you have ```python-telegram-module``` installed
+
+## Objectives
+- [x] Create List
+- [x] Show All Lists
+- [x] Help command
+- [x] Create events to lists
+- [x] Delete events from lists
+- [ ] Show lists individually
+- [ ] Deploy on Heroku
+- [ ] Notify about day events
+- [ ] Persist lists
+
+## Steps to code
+* Activate Venv ```source env/bin/activate.sh```
+* Install module ```pip install python-telegram-module```
+* Run server ```python main.py```
+* Export Token ```export BOT_API_TOKEN="bot_token"```
+* Import it ```import os```
+* Declare it on a variable ```token = os.environ['BOT_API_TOKEN']```
+
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 # coding: utf-8
-import sys
-
+import sys, os
 from telegram import ParseMode
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # All lists
 all_lists = []
 
 # check for new messages --> polling
-updater = Updater(token="818262027:AAHwNrZal6dNo4fgYugGNZewtU2knok61-g")
+# token = os.environ['BOT_API_TOKEN']
+token = ''
+updater = Updater(token=token)
 
 # allows to register handler --> command, text, video, audio, etc.
 dispatcher = updater.dispatcher
@@ -27,7 +28,8 @@ def help(bot, update):
              '\n\n"/start" para iniciar uma conversa com o Bot'
              '\n"/help" para listar os comandos'
              '\n"/listar" para exibir as listas'
-             '\n"/criarLista" para criar novas listas',
+             '\n"/criarLista NomeDaLista" para criar novas listas'
+             '\n"/criarEvento NomeDaLista NomeDoEvento" para criar novos eventos',
         parse_mode=ParseMode.MARKDOWN
     )
 
@@ -36,14 +38,18 @@ def listar(bot, update):
     if len(all_lists) == 0:
         bot.send_message(
             chat_id=update.message.chat_id,
-            text='Você não possui *nenhuma* lista.\nQue tal criar uma? Utilize o comando "/criarLista" para criar uma nova lista.',
+            text='Você não possui *nenhuma* lista.\nQue tal criar uma? Utilize o comando "/criarLista" passando um nome para criar uma nova lista.\nExemplo: "/criarLista A Fazeres"',
             parse_mode=ParseMode.MARKDOWN
         )
 
     else:
         mensagem = ""
         for i in range(len(all_lists)):
-            mensagem += "* %s :*\n" % all_lists[i]["nome"]
+            mensagem += "* {}:*".format(all_lists[i]["nome"])
+            for j in all_lists[i]["itens"]:
+                mensagem += "\n   • {}".format(j)
+            mensagem += "\n\n"
+
         bot.send_message(
             chat_id=update.message.chat_id,
             text=mensagem,
@@ -52,31 +58,133 @@ def listar(bot, update):
 
 
 def criarlista(bot, update, args):
-    user_says = " ".join(args)
-    update.message.reply_text("You said: " + user_says)
+    nome_lista = ' '.join(args).strip()
+
+    if nome_lista != "":
+        for i in range(len(all_lists)):
+            if nome_lista in all_lists[i]["nome"]:
+                bot.send_message(
+                    chat_id=update.message.chat_id,
+                    text='Uma lista com esse nome já foi criada.\nTente novamente utilizando outro nome.'
+                )
+                return None
+        bot.send_message(
+            chat_id=update.message.chat_id,
+            text='A lista {} será criada.'.format(nome_lista)
+        )
+        lista = {
+            "nome": nome_lista,
+            "itens": []
+        }
+        all_lists.append(lista)
+        bot.send_message(
+            chat_id=update.message.chat_id,
+            text="Lista criada com sucesso!"
+        )
+
+
+    else:
+        bot.send_message(
+            chat_id=update.message.chat_id,
+            text='Nome Inválido.\nTente novamente utilizando outro nome.'
+        )
+
+
+def criarevento(bot, update, args):
+    param = ' '.join(args).strip()
+    nome_lista = param.split(' ', 1)[0]
+    nome_evento = param.split(' ', 1)[1]
+
+    if nome_lista != "" and nome_evento != "":
+        listas = ""
+        for i in range(len(all_lists)):
+            listas += "\n" + all_lists[i]["nome"]
+            if nome_lista == all_lists[i]["nome"]:
+                if nome_evento not in all_lists[i]["itens"]:
+                    all_lists[i]["itens"].append(nome_evento)
+                    bot.send_message(
+                        chat_id=update.message.chat_id,
+                        text='Evento foi criado com sucesso.'
+                    )
+                    break
+                else:
+                    bot.send_message(
+                        chat_id=update.message.chat_id,
+                        text='Evento já existe.'
+                    )
+
+            if nome_lista != all_lists[i]["nome"] and i == len(all_lists)-1:
+                bot.send_message(
+                    chat_id=update.message.chat_id,
+                    text='Lista não existe. Essas são as listas disponíveis:\n{}'.format(listas)
+                )
+                return None
+
+    else:
+        bot.send_message(
+            chat_id=update.message.chat_id,
+            text='Nome da lista ou nome do evento inválido.'
+        )
+
+
+def deletarevento(bot, update, args):
+    param = ' '.join(args).strip()
+    nome_lista = param.split(' ', 1)[0]
+    nome_evento = param.split(' ', 1)[1]
+
+    if nome_lista != "" and nome_evento != "":
+        listas = ""
+        for i in range(len(all_lists)):
+            listas += "\n" + all_lists[i]["nome"]
+            if nome_lista == all_lists[i]["nome"]:
+                if nome_evento in all_lists[i]["itens"]:
+                    index = all_lists[i]["itens"].index(nome_evento)
+                    all_lists[i]["itens"].pop(index)
+                    bot.send_message(
+                        chat_id=update.message.chat_id,
+                        text='Evento foi deletado com sucesso.'
+                    )
+                    break
+                else:
+                    bot.send_message(
+                        chat_id=update.message.chat_id,
+                        text='Evento não existe ou já foi deletado.'
+                    )
+
+            if nome_lista != all_lists[i]["nome"] and i == len(all_lists)-1:
+                bot.send_message(
+                    chat_id=update.message.chat_id,
+                    text='Lista não existe. Essas são as listas disponíveis:\n{}'.format(listas)
+                )
+                return None
+
+    else:
+        bot.send_message(
+            chat_id=update.message.chat_id,
+            text='Nome da lista ou nome do evento inválido.'
+        )
+
+
+def unknown(bot, update):
     bot.send_message(
         chat_id=update.message.chat_id,
-        text='Vamos criar uma nova lista. Qual o nome de sua nova lista?'
-    )
-    print "Nome da lista %s" % user_says
-    lista = {"nome": user_says}
-    print "Lista criada"
-    all_lists.append(lista)
-    print "Lista adicionada"
-    print all_lists
-    bot.send_message(
-        chat_id=update.message.chat_id,
-        text="Lista criada com sucesso!"
+        text='Comando não encontrado.\nDigite "/help" para listar todos os comandos.'
     )
 
 
 # create a command handler
 start_handler = CommandHandler("start", start)
-listar_handler = CommandHandler("listar", listar)
-criarLista_handler = CommandHandler("criarLista", criarlista, pass_args=True)
 help_handler = CommandHandler("help", help)
+unknown_handler = MessageHandler([Filters.command], unknown)
 
-handlers = [start_handler, listar_handler, criarLista_handler, help_handler]
+listar_handler = CommandHandler("listar", listar)
+
+criarLista_handler = CommandHandler("criarLista", criarlista, pass_args=True)
+criarEvento_handler = CommandHandler("criarEvento", criarevento, pass_args=True)
+
+deletarevento_handler = CommandHandler("deletarEvento", deletarevento, pass_args=True)
+
+handlers = [start_handler, listar_handler, deletarevento_handler, criarLista_handler, criarEvento_handler, help_handler, unknown_handler]
 
 # add command handler to dispatcher
 for i in handlers:

518d46e9ad1792821e3c293652007f0df715be99
Gustavo Santos <38411510+gustavolbs@users.noreply.github.com>
2019-04-02 10:18:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ except:
 
 
 # check for new messages --> polling
-token = '818262027:AAHwNrZal6dNo4fgYugGNZewtU2knok61-g'
+token = ''
 updater = Updater(token=token)
 
 # allows to register handler --> command, text, video, audio, etc.
